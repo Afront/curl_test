c1f91b889df800033589d4524a4dc96edc0da233
Afront <3943720+Afront@users.noreply.github.com>
2020-05-07 23:44:12 +0900

    Update curl_test

--- src/curl_test.cr
+++ src/curl_test.cr
@@ -1,6 +1,8 @@
 # TODO: Write documentation for `CurlTest`
 module CurlTest
-  VERSION = "0.1.0"
+  VERSION  = "0.1.0"
+  DATA     = "1" * 1000
+  DATA_URL = "#{DATA}&other_data=#{DATA}&another_data=#{DATA}&data1=#{DATA}&data0=#{DATA}&data2=#{DATA}&data3=#{DATA}"
 
   def self.get_usage
     "Usage: curl_test host count size port"
@@ -8,24 +10,31 @@ module CurlTest
 
   # TODO: Put your code here
 
-  abort get_usage unless ARGV.size == 4
+  # abort get_usage unless ARGV.size == 4
 
-  puts ARGV
-  host, count, size, port = ARGV
-  i = count.to_i - 1
-  while i > 0
-    spawn do
-      command = %Q{ curl http://#{host}:#{port}/#{size}_test.html -w "%{time_connect},%{time_total},%{speed_download},%{http_code},%{size_download},%{url_effective}\n" -o /dev/null -s >> myResults.csv | bash}
-      args = ["-c"]
-      args << command
+  # puts ARGV
+  # host, count, size, port = ARGV
 
-      Process.run("sh", args)
+  def self.run_curl(mode : String, url : String)
+    100.times do |i|
+      spawn do
+        command = %Q{ curl --data "#{DATA_URL}" --#{mode.strip} #{url.strip}/post -w "%{time_connect},%{time_total},%{speed_download},%{http_code},%{size_download}\n" -o /dev/null -s >> #{mode.strip}.csv | bash}
+        args = ["-c"]
 
-      sleep 0.1.seconds
+        Process.run("sh", args << command)
+
+        sleep 0.1.seconds
+      end
     end
-    i -= 1
+
+    Fiber.yield
+    sleep 0.1.seconds
   end
 
-  Fiber.yield
-  sleep 10.seconds
+  run_curl("http3", "https://quic.aiortc.org/httpbin")
+  run_curl("http2-prior-knowledge", "https://httpbin.org")
+  run_curl("http2", "https://httpbin.org")
+  run_curl("http1.1", "https://httpbin.org")
+  run_curl("http1.0", "https://httpbin.org")
+  run_curl("http0.9", "https://httpbin.org")
 end
